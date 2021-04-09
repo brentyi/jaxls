f6c4a172d62c6b32c23b2d7f1895a77e67c4a78a
Brent Yi <yibrenth@gmail.com>
2021-04-09 13:24:15 -0700

    Expand marginalization test

--- tests/test_marginalization.py
+++ tests/test_marginalization.py
@@ -64,32 +64,59 @@ def test_marginalization_double():
 
 def test_marginalization_as_dense():
 
-    pose_variable = jaxfg.geometry.SE2Variable()
-    pose_value = jaxlie.SE2.identity()
+    pose_variables = [
+        jaxfg.geometry.SE2Variable(),
+        jaxfg.geometry.SE2Variable(),
+    ]
 
-    sqrt_cov = onp.random.randn(3, 3)
-    cov = sqrt_cov @ sqrt_cov.T
-    scale_tril_inv = onp.linalg.inv(onp.linalg.cholesky(cov))
+    factors: List[jaxfg.core.FactorBase] = [
+        jaxfg.geometry.PriorFactor.make(
+            variable=pose_variables[0],
+            mu=jaxlie.SE2.from_xy_theta(0.0, 0.0, 0.0),
+            scale_tril_inv=onp.eye(3),
+        ),
+        jaxfg.geometry.PriorFactor.make(
+            variable=pose_variables[1],
+            mu=jaxlie.SE2.from_xy_theta(2.0, 0.0, 0.0),
+            scale_tril_inv=onp.eye(3),
+        ),
+        jaxfg.geometry.BetweenFactor.make(
+            variable_T_world_a=pose_variables[0],
+            variable_T_world_b=pose_variables[1],
+            T_a_b=jaxlie.SE2.from_xy_theta(1.0, 0.0, 0.0),
+            scale_tril_inv=onp.eye(3),
+        ),
+    ]
 
-    graph = jaxfg.core.StackedFactorGraph.make(
-        factors=[
-            jaxfg.geometry.PriorFactor.make(
-                variable=pose_variable, mu=pose_value, scale_tril_inv=scale_tril_inv
-            ),
-            jaxfg.geometry.PriorFactor.make(
-                variable=pose_variable, mu=pose_value, scale_tril_inv=scale_tril_inv
-            ),
-        ]
+    graph = jaxfg.core.StackedFactorGraph.make(factors)
+    initial_assignments = jaxfg.core.VariableAssignments.make_from_defaults(
+        pose_variables
     )
-    assignments = jaxfg.core.VariableAssignments.make_from_dict(
-        {pose_variable: pose_value}
+
+    solution_assignments = graph.solve(initial_assignments)
+
+    sparse_covariance = jaxfg.experimental.SparseCovariance.make(
+        graph, solution_assignments
     )
 
-    covariance = jaxfg.experimental.SparseCovariance.make(graph, assignments)
+    sqrt_information_matrix = graph.compute_residual_jacobian(
+        solution_assignments
+    ).as_dense()
 
+    covariance0 = onp.linalg.inv(sqrt_information_matrix.T @ sqrt_information_matrix)
+    covariance1 = sparse_covariance.as_dense(use_inverse=False)
+    covariance2 = sparse_covariance.as_dense(use_inverse=True)
+
+    assert covariance0.shape == (6, 6)
     onp.testing.assert_allclose(
-        covariance.as_dense(use_inverse=False),
-        covariance.as_dense(use_inverse=True),
+        covariance0,
+        covariance1,
+        atol=1e-8,
+        rtol=1e-5,
+    )
+    onp.testing.assert_allclose(
+        covariance1,
+        covariance2,
         atol=1e-8,
         rtol=1e-5,
     )
