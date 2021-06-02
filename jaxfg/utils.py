c6cbb9ff725274c7788fc7058265f3f780540fd3
Brent Yi <yibrenth@gmail.com>
2021-06-02 01:39:35 -0700

    Remove pytree registration, tweak mypy.ini

--- jaxfg/utils.py
+++ jaxfg/utils.py
@@ -1,9 +1,8 @@
 import contextlib
 import time
-from typing import Generator, Type, TypeVar
+from typing import Generator, TypeVar
 
 import jax
-import jax_dataclasses
 import termcolor
 from jax import numpy as jnp
 
@@ -29,21 +28,3 @@ def stopwatch(label: str = "unlabeled block") -> Generator[None, None, None]:
     yield
     print(f"{termcolor.colored(str(time.time() - start_time), attrs=['bold'])} seconds")
     print("========")
-
-
-def _identity(x: T) -> T:
-    return x
-
-
-def register_dataclass_pytree(cls: Type[T]) -> Type[T]:
-    """Legacy API. Deprecation warning for now. TODO: remove this"""
-
-    import warnings
-
-    warnings.warn(
-        "@register_dataclass_pytree has been phased out -- use"
-        " @jax_dataclasses.pytree_dataclass() instead!",
-        DeprecationWarning,
-        stacklevel=1,
-    )
-    return jax_dataclasses._dataclasses._register(cls)
